8d2b6548ed5b88d564a5540c8a3fe04521057a8c
volosgoto <volosgoto@yahoo.com>
2019-04-08 10:02:43 +0300

    fix ===

--- src/Counter.js
+++ src/Counter.js
@@ -1,32 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Counter extends Component {
   constructor(props) {
     super(props);
     this.state = {
       currentNumber: this.props.initialNumber
-    }
+    };
   }
 
   increment = () => {
-    return this.setState((prevState) => ({
+    return this.setState(prevState => ({
       currentNumber: prevState.currentNumber + 1
-    }))
-  }
+    }));
+  };
 
   decrement = () => {
-    return this.setState((prevState) => ({
-      currentNumber: (this.props.initialNumber == prevState.currentNumber) ? this.props.initialNumber : prevState.currentNumber - 1
-    })
-    );
-  }
+    return this.setState(prevState => ({
+      currentNumber:
+        this.props.initialNumber === prevState.currentNumber
+          ? this.props.initialNumber
+          : prevState.currentNumber - 1
+    }));
+  };
 
   reset = () => {
     return this.setState(() => ({
       currentNumber: 0
-    }
-    ))
-  }
+    }));
+  };
 
   // Alternative
   // increment = () => {
@@ -36,17 +37,15 @@ export default class Counter extends Component {
   // }
 
   render() {
-    console.log(this.state)
+    console.log(this.state);
     return (
-
       <div>
         <h1>Counter App</h1>
         <p>{this.state.currentNumber}</p>
         <button onClick={this.increment}>Increment</button>
         <button onClick={this.decrement}>Decrement</button>
         <button onClick={this.reset}>Reset</button>
-
-      </div >
-    )
+      </div>
+    );
   }
 }
